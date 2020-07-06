f859058acc9e183a20d1bad5368e7bdc70c53882
pmironn472 <63416798+pmironn472@users.noreply.github.com>
2020-07-06 16:56:01 +0300

    Add files via upload

--- src/AnnotationsApp.java
+++ src/AnnotationsApp.java
@@ -4,14 +4,11 @@ import java.awt.*;
 
 public class AnnotationsApp {
     static int count = 111001;
+
     public static void main(String[] args) {
 
 
 //EDD -event (Action -> reaction)
-        TextField text = new TextField(20);
-        var ref = new Object() {
-            private int numClicks = 0;
-        };
         JFrame window = new JFrame("My first GUI");
         window.setSize(500, 500);
         window.show();
@@ -19,12 +16,13 @@ public class AnnotationsApp {
         JButton button = new JButton("Click!");
         window.getContentPane().add(button);
         button.addActionListener(e -> {
-            if(count <=999) {
+            if (count <= 999) {
                 button.setText(String.format("%d Likes", count));
-            }else if( count > 1000 && count<=100000){
-                button.setText(String.format("%1.1fK Likes", (float)count/1000));
-            }else if( count > 100001){
-                button.setText(String.format("%1.1fM Likes", (float)count/10000));}
+            } else if (count > 1000 && count <= 100000) {
+                button.setText(String.format("%1.1fK Likes", (float) count / 1000));
+            } else if (count > 100001) {
+                button.setText(String.format("%1.1fM Likes", (float) count / 10000));
+            }
             count++;
         });
 
